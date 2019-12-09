df4153da66630c24e0990cbd96775170a51b0b64
candle-tikpot <58674819+candle-tikpot@users.noreply.github.com>
2019-12-09 10:46:34 +0800

    First Commit
    School Test

---
+++ firebase_config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDXM_wGAJOLsf11BTlznqknnGzbS2xeVHg",
+    authDomain: "community-5d362.firebaseapp.com",
+    databaseURL: "https://community-5d362.firebaseio.com",
+    projectId: "community-5d362",
+    storageBucket: "community-5d362.appspot.com",
+    messagingSenderId: "1098909737635",
+    appId: "1:1098909737635:web:bcbdcf4142c5924d6c9516",
+    measurementId: "G-K4B1HSL1E6"
+};
+
+firebase.initializeApp(firebaseConfig);
